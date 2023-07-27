caad110f8e604202551fb9bc0fccb52e57382877
Mujib <mujib@Mujibs-MacBook-Pro.local>
2023-07-27 09:29:06 +0530

    js day 1

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log('Hello world');
+
+var age = 20;
+var name = "John";
+var isEmployed = true;
+
+// assignment
+// images, videos, text
+// Primary memory: RAM, temp
+// secondary memory: HDD, SSD, perm
+// Processor
+// screen
+// keyboard, mouse
+// General purpose 
+// Java, C#, Python, JS, Scala 
+// programming
+//  output statement
+// input: marks
+// process: calculate
+// output: agg
+// resusability 
+// variables & data types
---
+++ index2.js
@@ -0,0 +1,16 @@
+// write program to add two numbers and print the result
+// operator
+
+// declaration 
+// initialization (assigning)
+
+// declaration
+// JS runtime
+var a = 150; // 64bits, 8 bytes
+var b = 20; 
+
+var x = 9; // 0000 0000 0000 0000 0000 0000 0000 1001
+
+var c = a + b;
+
+console.log("result is", c);
